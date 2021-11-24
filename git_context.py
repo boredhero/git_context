590e037b91a667b55fb05fff922c1b5b2d8cd93b
Noah Martino <noah@freedomgateway.llc>
2021-11-24 03:24:30 -0500

    get started on list command

--- git_context.py
+++ git_context.py
@@ -9,9 +9,16 @@ def main():
     elif len(args) == 2 and args[1] == "help":
         gc_help()
         exit()
-
+    elif len(args) == 2 and args[1] == "list":
+        gc_list()
+        exit()
+    else:
+        gc_help()
+        exit()
 def gc_list():
-    pass
+    data = read_data()
+    if data["Contexts"] is None:
+        print("There are currently no contexts. To see how to get started, see 'git_context help'\n")
 def gc_add():
     pass
 def gc_create():
@@ -35,6 +42,33 @@ def gc_help():
     except Exception as e:
         print("An exception occured while trying to print help. Please report this at https:/github.com/boredhero/git_context\n Stacktrace:\n")
         print(e)
+
+## Util methods
+def read_data():
+    '''
+    Return data as dictionary
+    
+    :returns: {}
+    Will exit program if an error occured trying to create data.yml for the first time
+    '''
+    try:
+        data = bios.read("data.yml", file_type="yaml")
+        data = dict(data)
+        return data
+    except FileNotFoundError:
+        d = {"Contexts": None}
+        try:
+            bios.write("data.yml", d, file_type="yaml")
+            data = bios.read("data.yml", file_type="yaml")
+            return data
+        except Exception as e:
+            print("An unknown issue occured trying to write data.yml. Please report this at https://github.com/boredhero/git_context\n Stacktrace: \n")
+            print(e)
+            exit()
+    except Exception as e:
+        print("An unknown error occured\n Stacktrace: \n")
+        print(e)
+        exit
 
 if __name__ == "__main__":
     main()
