89a08a57ac9b9d9b8b32bd08bb5398c834a47b02
Noah <personal.boredhero@gmail.com>
2021-12-29 23:00:11 -0500

    Remove bugs in exit flow and work on git-context add

--- git_context.py
+++ git_context.py
@@ -9,30 +9,52 @@ def main():
     print(args)
     if len(args) == 1:
         gc_help()
-        exit()
+        exit(0)
     elif len(args) == 3 and args[1] == "help":
         gc_help()
-        exit()
+        exit(0)
     elif len(args) == 3 and args[1] == "list":
         gc_list()
-        exit()
+        exit(0)
     elif args[1] == "create":
         if len(args) < 6:
             print("More args required for git create")
-            exit()
+            exit(0)
         else:
             gc_create(args)
-            exit()
+            exit(0)
     else:
         gc_help()
-        exit()
+        exit(0)
 def gc_list():
     data = read_data()
     if data["Contexts"] is None:
         print("There are currently no contexts. To see how to get started, see 'git_context help'\n")
 def gc_add(args):
     username = args[2]
-    pass
+    email = args[3]
+    key_type = args[4]
+    filepath = args[5]
+    if key_type != "ed25519" and key_type != "rsa":
+        print(f"Error: git-context add: Key types ed25519 and rsa are valid, you entered {key_type}")
+        exit(1)
+    if username is None or username is "" or username is " ":
+        print("Error: git-context add: You did not specify a git username. Aborting.")
+        exit(1)
+    if email is None or email is "" or email is " ":
+        print("Error: git-context add: You did not specify an email. Aborting.")
+        exit(1)
+    if filepath is None or filepath is "" or filepath is " ":
+        print("Error: git-context add: You did not specify a file path. Aborting.")
+        exit(1)
+    if os.path.isfile(filepath) is False:
+        print(f"Error: git-context add: Unable to find specified keyfile ('{filepath}'). Aborting.")
+        exit(1)
+    # If we've passed all these checks, we're clear to add it to our data storage
+    data = read_data()
+    data["Contexts"][username] = {"email": email, "key_type": key_type, "username": username, "filename": filepath}
+    write_data(data)
+    exit(0)
 def gc_create(args):
     username = args[2]
     email = args[3]
@@ -40,11 +62,12 @@ def gc_create(args):
     filename = generate_ssh_keypair(email, key_type)
     if filename is None:
         print("Error creating keys for git-context create. Aborting. Please manually create keys and use git-context add")
+        exit(1)
     else:
         data = read_data()
         data["Contexts"][username] = {"email": email, "key_type": key_type, "username": username, "filename": filename}
         write_data(data)
-    exit()
+    return
 def gc_delete(args):
     pass
 def gc_get(args):
@@ -62,11 +85,13 @@ def gc_help(args):
         print(f"{info['pretty_name']} - v{info['version']} '{info['version_nickname']}' by {info['author']}\n")
         print(help_text)
         help_file.close()
+        return
     except Exception as e:
         print("An exception occured while trying to print help. Please report this at https:/github.com/boredhero/git_context\n Stacktrace:\n")
         print(e)
         path = os.getcwd()
         print(f"Current Working Directory: {path}")
+        exit(1)
 
 ## Util methods
 def read_global_paths():
@@ -82,7 +107,7 @@ def read_global_paths():
     except Exception as e:
         print("An error occured trying to find ~/.git_context_global.yml file. Aborting.\nStacktrace: \n")
         print(e)
-        exit()
+        exit(1)
 def read_data():
     '''
     Return data as dictionary
@@ -105,11 +130,11 @@ def read_data():
         except Exception as e:
             print("An unknown issue occured trying to write data.yml. Please report this at https://github.com/boredhero/git_context\n Stacktrace: \n")
             print(e)
-            exit()
+            exit(1)
     except Exception as e:
         print("An unknown error occured\n Stacktrace: \n")
         print(e)
-        exit
+        exit(1)
 
 def write_data(d):
     '''
@@ -122,14 +147,15 @@ def write_data(d):
             d = dict(d)
         except Exception as e:
             print(e)
-            exit()
+            exit(1)
     try:
         paths = read_global_paths()
         bios.write(paths["data_path"], d, file_type="yaml")
+        return
     except Exception as e:
         print("An unknown error occured while trying to write data\n Stacktrace: \n")
         print(e)
-        exit()
+        exit(1)
 
 def generate_ssh_keypair(email, key_type="ed25519"):
     """
